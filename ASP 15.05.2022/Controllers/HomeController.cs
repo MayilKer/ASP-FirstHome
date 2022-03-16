8e93015e6ecdb7beed948eb59f4d0be7691f9af9
Mayil Kerimov <mayilik@code.edu.az>
2022-03-16 13:17:17 +0400

    Frist

---
+++ ASP 15.05.2022/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_15._05._2022.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
